713db990e3c3781b4239a2ca626eb14634f30cc1
vsyniohub <storm6594@gmail.com>
2023-11-08 22:28:39 +0100

    Current step logic

--- force-app/main/default/lwc/gameInstance/gameInstance.js
+++ force-app/main/default/lwc/gameInstance/gameInstance.js
@@ -4,15 +4,15 @@ import getSession from "@salesforce/apex/GameSessionController.getSession";
 export default class GameInstance extends LightningElement {
     @api code;
 
-    units;
-    response;
+    response = {};
+    currentUnit = {};
     async connectedCallback() {
         try {
             const retrieveResult = await getSession({
                 sessionCode : this.code
             });
             this.response = retrieveResult;
-            this.units = retrieveResult?.units;
+            this.currentUnit = this.response.currentUnit;
         } catch (error) {
             console.log(error);
         }
